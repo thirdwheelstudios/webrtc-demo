102a12fe8f27f601669e0a778f1c7e3f6b74d006
Phil Crossland <phil@thirdwheelstudios.com>
2022-09-26 21:18:41 +0100

    Added connect method and message data callback

--- src/composables/useScaledrone.ts
+++ src/composables/useScaledrone.ts
@@ -1,15 +1,31 @@
-import { onMounted, ref } from "vue"
+import { ref } from "vue"
 
 export const useScaledrone = () => {
+  let drone: Scaledrone
+  let room: Room
   const isConnected = ref(false)
+  const hasJoined = ref(false)
+  const messages = ref<any[]>([])
 
-  onMounted(() => {
-    const drone = new Scaledrone(import.meta.env.VITE_CHANNEL_ID)
+  const joinRoom = (roomName: string) => {
+    room = drone.subscribe(roomName)
+
+    room.on("open", (error) => {
+      hasJoined.value = !error
+    })
+
+    room.on("data", message => {
+        messages.value.push(message)
+    })
+  }
+
+  const connect = () => {
+    drone = new Scaledrone(import.meta.env.VITE_CHANNEL_ID)
 
     drone.on("open", (error) => {
       isConnected.value = !error
     })
-  })
+  }
 
-  return { isConnected }
+  return { connect, isConnected, joinRoom, hasJoined, messages }
 }

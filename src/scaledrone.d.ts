b208bdce5f10d46768c35eb4ba3dfbdb2a4f3ffe
Phil Crossland <phil@thirdwheelstudios.com>
2022-09-26 21:14:33 +0100

    Added more types

--- src/scaledrone.d.ts
+++ src/scaledrone.d.ts
@@ -1,6 +1,15 @@
-declare class Scaledrone {
-  on(arg0: string, arg1: (error: any) => void) {
-      throw new Error("Method not implemented.");
+declare class BaseScaledrone {
+  on(arg0: string, arg1: (data: any) => void) {
+    throw new Error("Method not implemented.")
   }
+}
+
+declare class Room extends BaseScaledrone {
+  name: string
+}
+
+declare class Scaledrone extends BaseScaledrone {
+  subscribe(roomName: string): Room
+
   constructor(channelId: string)
 }
